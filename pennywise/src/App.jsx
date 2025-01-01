da28ac6d5e0d65eb2d1c82f5d9cac4ca8bd3d3e5
Taimoor <taimoor2003abrejo@gmail.com>
2025-01-01 16:35:40 +0500

    Added Tailwind

--- pennywise/src/App.jsx
+++ pennywise/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <h1 className= "text-7xl text-red-500 bg-[#D5D8DC]">First Project</h1>
+    {/* <div className='Taimoor'></div> */}
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +29,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
