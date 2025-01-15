c88ef8f8a0804352d18fdd64ff1671029e0f7107
Taimoor <taimoor2003abrejo@gmail.com>
2025-01-15 15:22:37 +0500

    Made the Log In page

---
+++ pennywise/src/pages/Signup.jsx
@@ -0,0 +1,7 @@
+const Signup = () => {
+  return (
+    <div className="flex justify-center items-center bg-background min-h-screen"></div>
+  );
+};
+
+export default Signup;
--- pennywise/tailwind.config.js
+++ pennywise/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2563eb", // Default Blue (Changeable)
+        secondary: "#9333ea", // Purple (Changeable)
+        background: "#f3f4f6", // Light Gray (Changeable)
+        text: "#1f2937",
+      },
+      fontFamily: {
+        'pennywise': ['DistilleryScript']
+      }
+    },
   },
   plugins: [],
 }
