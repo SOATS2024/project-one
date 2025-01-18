7e77ce6adf1ea09b8a55c97e89f7b7446751af0e
MuhammadSuhaib01 <muhammadsuhaib7878@gmail.com>
2025-01-19 01:02:08 +0500

    issues resolved

--- pennywise/src/pages/Dashboard.jsx
+++ pennywise/src/pages/Dashboard.jsx
@@ -1,46 +1,43 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { useFirebase } from "../context/firebase"
+import { useFirebase } from "../context/firebase";
 import ExpenseForm from "../components/ExpenseInput";
 import ExpenseList from "../components/ExpenseList";
 import Navbar from "../components/Navbar";
 
-export default function Dashboard() {
-    const navigate = useNavigate();
-    const {logOut} = useFirebase();
-    const [error, setError] = useState(null);
+const Dashboard = () => {
+  const navigate = useNavigate();
+  const { logOut } = useFirebase();
+  const [error, setError] = useState(null);
 
-    const handleSubmit = async () =>{
-
-      try{
-        await logOut()
-        navigate("/Login")
-      } catch {
-        setError("An error occurred while Signing Out")
-      }
+  const handleSubmit = async () => {
+    try {
+      await logOut();
+      navigate("/Login");
+    } catch {
+      setError("An error occurred while Signing Out");
     }
+  };
 
-
-    return (
+  return (
     <>
-    // <div className="grid grid-cols-3 gap-2 items-center">
-    //   <div>Hello world!</div>
-    //   <div>Hello world!</div>
-    //   <div>
-    //     <ExpenseForm />
-    //   </div>
-    // </div>
-      
-    <div>Dashboard</div>
-    <h1>Click the button below:</h1>
-    <button onClick={handleSubmit}>Sign Out</button>
-    <div className="bg-background min-h-screen">
-      <Navbar isWelcome={false} />
-      <ExpenseForm />
-      <ExpenseList />
-    </div>
+      {/* <div className="grid grid-cols-3 gap-2 items-center">
+        <div>Hello world!</div>
+        <div>Hello world!</div>
+        <div>
+          <ExpenseForm />
+        </div>
+      </div> */}
+      <div>Dashboard</div>
+      <h1>Click the button below:</h1>
+      <button onClick={handleSubmit}>Sign Out</button>
+      <div className="bg-background min-h-screen">
+        <Navbar isWelcome={false} />
+        <ExpenseForm />
+        <ExpenseList />
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Dashboard;
