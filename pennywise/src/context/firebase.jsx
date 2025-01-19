c1909903f6b21148d6af8f5eee172843ce423061
J1nSakai <taimoor2003abrejo@gmail.com>
2025-01-20 00:36:55 +0500

    fixed some issues, i guess

--- pennywise/src/context/firebase.jsx
+++ pennywise/src/context/firebase.jsx
@@ -22,12 +22,12 @@ import {
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD87WXfVYRUirN0p7q9d2daWMT07ntNaIQ",
-  authDomain: "pennywise-9f005.firebaseapp.com",
-  projectId: "pennywise-9f005",
-  storageBucket: "pennywise-9f005.appspot.com",
-  messagingSenderId: "478808999063",
-  appId: "1:478808999063:web:35d1469217532149e2d5d2",
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 };
 
 const FirebaseContext = createContext(null);
